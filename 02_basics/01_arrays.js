61057bcf21b198598fbcf510b31c74c53fc3f7c5
Rajiv Kumar Ranjan <RRTRADE67@GMAIL.COM>
2026-04-25 21:20:42 +0530

    pop push

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,17 +1,23 @@
-console.log("Arrays in JavaScript");
+// console.log("Arrays in JavaScript");
 //array
 
 const myArr=[1,2,3,4,5];
 
-console.log(myArr[3]); //4
-console.log(myArr)
+// console.log(myArr[3]); //4
+// console.log(myArr)
 
 const MyHeros=['Superman','Batman','Spiderman','Ironman','Thor'];
 
-console.log(MyHeros); //Superman
-console.log(MyHeros[2]); //Spiderman
+// console.log(MyHeros); //Superman
+// console.log(MyHeros[2]); //Spiderman
 
 const myArr2=new Array(1,2,3,4,5);
 
-console.log(myArr2); //[1, 2, 3, 4, 5]
-console.log(myArr2[2]); //3  
+// console.log(myArr2); //[1, 2, 3, 4, 5]
+// console.log(myArr2[2]); //3  
+
+//Array methods.
+myArr.push(9)
+myArr.push(8)
+myArr.pop()
+console.log(myArr)
