8afe58cca9d59ec3f2b7e1c70627c9b589b0f0c5
Rajiv Kumar Ranjan <RRTRADE67@GMAIL.COM>
2026-04-25 15:34:11 +0000

    learn

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+//array
+
+const myArr=[1,2,3,4,5];
+
+console.log(myArr[3]); //4
+console.log(myArr)
+
+const MyHeros=['Superman','Batman','Spiderman','Ironman','Thor'];
+
+console.log(MyHeros); //Superman
+console.log(MyHeros[2]); //Spiderman
+
+const myArr2=new Array(1,2,3,4,5);
+
+console.log(myArr2); //[1, 2, 3, 4, 5]
+console.log(myArr2[2]); //3
